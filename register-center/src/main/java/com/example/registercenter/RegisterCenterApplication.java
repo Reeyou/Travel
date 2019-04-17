10300f9e4ac34d9a3fdb3573afe0885cbc6c98ac
Reeyou <ryu993830137@163.com>
2019-04-17 10:50:15 +0800

    '更新注册、配置中心新增user数据库表'

---
+++ register-center/src/main/java/com/example/registercenter/RegisterCenterApplication.java
@@ -0,0 +1,18 @@
+package com.example.registercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+	注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCenterApplication.class, args);
+	}
+
+}
